6222e6deed5e4b3f0b55a0573d3e6057c6bfd757
Julien Adam <julien.adam@gmail.com>
2023-01-19 18:38:53 +0100

    Initial commit

---
+++ NorthwindSqliteAdoNet/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace NorthwindSqliteAdoNet
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var conn = new SqliteConnection(@"Data Source=.\northwind.db"))
+            {
+                conn.Open();
+                Console.WriteLine("Connection successful");
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "Select count(*) from employees";
+                var result = cmd.ExecuteScalar();
+                Console.WriteLine($"Number of employees : {result}");
+            }
+
+            Console.WriteLine("Success");
+        }
+    }
+}
